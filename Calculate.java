14b2d0560d3d7adb44602c01ddcab59d45799a80
alexeytrubnikov <alexely.trubnikov.88@gmail.com>
2017-05-07 22:21:35 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -4,6 +4,11 @@ public class Calculate{
 		
 		System.out.println("Calculate..");
 		
+		int first = Integer.valueOf(args[0]);
+		int second= Integer.valueOf(args[1]);
+		int summ = first + second;
+		System.out.println("sum: " + summ);
+		
 	}
 	
 }
